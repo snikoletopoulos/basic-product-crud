cef89a07a8dbf8c7e1b73f305875082fc701567b
Stavros <snikoletopoulos@hotmail.com>
2024-06-08 22:05:43 +0300

    chore: setup basic express

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
-import express from "express";
 import "dotenv/config";
 import "libs/env.js";
+import express from "express";
 
-import { get404 } from "controllers/error.controllers.js";
-import { productRouter } from "./routes/product.routes.js";
-import { errorMiddleware } from "middlewares/error.middleware.js";
 import { db } from "libs/db.js";
+import { errorMiddleware } from "middlewares/error.middleware.js";
+import { get404 } from "controllers/error.js";
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (_req, res) => {
-	res.send("Hello World");
-});
+app.use(express.json());
 
 app.use(productRouter);
 app.use(productRouter);
@@ -24,10 +19,10 @@ app.use(errorMiddleware);
 
 try {
 	await db.$connect();
+
+	app.listen(process.env.PORT, () => {
+		console.log(`[server]: Server is running at port ${process.env.PORT}`);
+	});
 } catch (error) {
 	console.error("Error connecting to database: ", error);
 }
-
-app.listen(process.env.PORT, () => {
-	console.log(`[server]: Server is running at port ${process.env.PORT}`);
-});
